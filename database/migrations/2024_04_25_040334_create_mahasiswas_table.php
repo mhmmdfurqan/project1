caf46dca2c1772d60e26b15f28159430e495ce2e
mhmmdfurqan <mhd.furqan76@gmail.com>
2024-04-25 12:07:44 +0700

    membuat table migration

--- database/migrations/2024_04_25_040334_create_mahasiswas_table.php
+++ database/migrations/2024_04_25_040334_create_mahasiswas_table.php
@@ -11,14 +11,14 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('mahasiswas', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->enum('hak_akses', ['admin', 'user'])->default('user');
-            $table->rememberToken();
+            $table->string('nim',50)->unique();
+            $table->string('tempatlahir',50);
+            $table->date('tanggallahir');
+            $table->string('jeniskelamin',50);
+            $table->string('agama',50);
+            $table->bigInteger('jurusans_id');
             $table->timestamps();
         });
     }
@@ -28,6 +28,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('mahasiswas');
     }
 };
