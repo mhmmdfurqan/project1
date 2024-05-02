0de5a69013eef459911aaf6827cd79a21cb173fa
mhmmdfurqan <mhd.furqan76@gmail.com>
2024-05-02 10:47:27 +0700

    penyesuaian tampilan home & login

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('master');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Auth::routes();
+
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
