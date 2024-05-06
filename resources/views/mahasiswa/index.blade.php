a2675cbce375a538a85aacf97ca7c02c3c04b48b
mhmmdfurqan <mhd.furqan76@gmail.com>
2024-05-06 07:40:35 +0700

    Penambahan Mahasiswa dan Jurusan

---
+++ resources/views/mahasiswa/index.blade.php
@@ -0,0 +1,98 @@
+@extends('layouts.master')
+@section('title','Data Mahasiswa')
+@section('heading','Data Mahasiswa')
+
+@section('bc')
+    <div class="col-sm-6">
+        <ol class="breadcrumb float-sm-right">
+        <li class="breadcrumb-item"><a href="#">Home</a></li>
+        <li class="breadcrumb-item active">Data Mahasiswa</li>
+        </ol>
+    </div>
+@endsection
+
+@section('content')
+    <div class="card">
+        <div class="card-header">
+        <h3 class="card-title">
+            <a href="#" class="btn btn-primary"><i class="fa fa-folder-plus"></i> Tambah Data</a>
+        </h3>
+
+        <div class="card-tools">
+            <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
+            <i class="fas fa-minus"></i>
+            </button>
+            <button type="button" class="btn btn-tool" data-card-widget="remove" title="Remove">
+            <i class="fas fa-times"></i>
+            </button>
+        </div>
+        </div>
+        <div class="card-body">
+            <table id="example1" class="table table-bordered table-striped">
+                <thead>
+                    <tr>
+                        <th>No</th>
+                        <th>Kode Mahasiswa</th>
+                        <th>Nama Mahasiswa</th>
+                        <th>Aksi</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Trident</td>
+                        <td>Internet Explorer 4.0</td>
+                        <td>Win 95+</td>
+                        <td>
+                            <a href="#" class="btn btn-info btn-sm"><i class="fa fa-pencil-alt"></i></a>
+                            <a href="#" class="btn btn-danger btn-sm"><i class="fa fa-trash"></i></a>
+                        </td>
+                    </tr>
+                </tfoot>
+            </table>
+        </div>
+        <!-- /.card-body -->
+
+    </div>
+@endsection
+
+@section('css')
+    <!-- DataTables -->
+    <link rel="stylesheet" href="{{ asset('plugins/datatables-bs4/css/dataTables.bootstrap4.min.css')}}">
+    <link rel="stylesheet" href="{{ asset('plugins/datatables-responsive/css/responsive.bootstrap4.min.css')}}">
+    <link rel="stylesheet" href="{{ asset('plugins/datatables-buttons/css/buttons.bootstrap4.min.css')}}">
+@endsection
+
+@section('js')
+    <!-- DataTables  & Plugins -->
+    <script src="{{ asset('plugins/datatables/jquery.dataTables.min.js')}}"></script>
+    <script src="{{ asset('plugins/datatables-bs4/js/dataTables.bootstrap4.min.js')}}"></script>
+    <script src="{{ asset('plugins/datatables-responsive/js/dataTables.responsive.min.js')}}"></script>
+    <script src="{{ asset('plugins/datatables-responsive/js/responsive.bootstrap4.min.js')}}"></script>
+    <script src="{{ asset('plugins/datatables-buttons/js/dataTables.buttons.min.js')}}"></script>
+    <script src="{{ asset('plugins/datatables-buttons/js/buttons.bootstrap4.min.js')}}"></script>
+    <script src="{{ asset('plugins/jszip/jszip.min.js')}}"></script>
+    <script src="{{ asset('plugins/pdfmake/pdfmake.min.js')}}"></script>
+    <script src="{{ asset('plugins/pdfmake/vfs_fonts.js')}}"></script>
+    <script src="{{ asset('plugins/datatables-buttons/js/buttons.html5.min.js')}}"></script>
+    <script src="{{ asset('plugins/datatables-buttons/js/buttons.print.min.js')}}"></script>
+    <script src="{{ asset('plugins/datatables-buttons/js/buttons.colVis.min.js')}}"></script>
+
+    <!-- Page specific script -->
+    <script>
+        $(function () {
+        $("#example1").DataTable({
+            "responsive": true, "lengthChange": false, "autoWidth": false,
+            "buttons": ["copy", "csv", "excel", "pdf", "print", "colvis"]
+        }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
+        $('#example2').DataTable({
+            "paging": true,
+            "lengthChange": false,
+            "searching": false,
+            "ordering": true,
+            "info": true,
+            "autoWidth": false,
+            "responsive": true,
+        });
+        });
+    </script>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\JurusanController;
+use App\Http\Controllers\MahasiswaController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,9 +16,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 // Route::get('/', function () {
-//     return view('welcome');
+//     return view('master');
+// });
+
+// Route::get('/tes-git', function () {
+//     return "Halo, ini cuma coba pakai git";
 // });
 
 Auth::routes();
 
 Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/jurusan/', [JurusanController::class, 'index']);
+
+Route::get('/mahasiswa/', [JurusanController::class, 'index']);
