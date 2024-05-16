c128d2a01353ccf9cf2235425faf89f8104194bf
mhmmdfurqan <mhd.furqan76@gmail.com>
2024-05-16 12:12:59 +0700

    proses update data jurusan (selesai) & proses hapus jurusan

--- resources/views/jurusan/index.blade.php
+++ resources/views/jurusan/index.blade.php
@@ -45,7 +45,31 @@
                         <td>{{$item->jurusan}}</td>
                         <td>                       
                             <a href="/jurusan/edit/{{$item->id}}" class="btn btn-info btn-sm"><i class="fa fa-pencil-alt"></i></a>
-                            <a href="/jurusan/hapus/" class="btn btn-danger btn-sm"><i class="fa fa-trash"></i></a>
+                            <button type="button" class="btn btn-danger btn-sm" data-toggle="modal" data-target="#hapus{{$item->id}}">
+                                <i class="fa fa-trash"></i>
+                            </button>
+                            <!-- Modal -->
+                            <div class="modal fade" id="hapus{{$item->id}}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                <div class="modal-dialog">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                    <h1 class="modal-title fs-5" id="exampleModalLabel">Peringatan</h1>
+                                    <button type="button" class="btn-close" data-dismiss="modal" aria-label="Close"></button>
+                                    </div>
+                                    <div class="modal-body">
+                                    YAkin Data Jurusan {{$item->jurusan}} di Hapus?
+                                    </div>
+                                    <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Batal</button>
+                                    <form action="/jurusan/{{$item->id}}" method="post">
+                                        @method('DELETE')
+                                        @csrf
+                                        <button type="submit" class="btn btn-primary">Hapus</button>
+                                    </form>
+                                    </div>
+                                </div>
+                                </div>
+                            </div>
                         </td>
                     </tr>
                     @empty
--- routes/web.php
+++ routes/web.php
@@ -32,5 +32,6 @@ Route::get('/jurusan/form', [JurusanController::class, 'create']);
 Route::post('/jurusan/store', [JurusanController::class, 'store']);
 Route::get('/jurusan/edit/{id}', [JurusanController::class, 'edit']);
 Route::put('/jurusan/{id}', [JurusanController::class, 'update']);
+Route::delete('/jurusan/{id}', [JurusanController::class, 'destroy']);
 
 Route::get('/mahasiswa/', [MahasiswaController::class, 'index']);
